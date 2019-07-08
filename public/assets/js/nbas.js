16df411c978afb175c7c66fd9f4a13e8a95d68b9
Phillip Chang <phillip-chang@live.com>
2019-07-07 23:17:49 -0700

    Add modal to confirm user choice

--- public/assets/js/nbas.js
+++ public/assets/js/nbas.js
@@ -2,25 +2,39 @@ $(function(){
     $(".change-trade").on("click", function(event){
         var id = $(this).data("id");
         var traded = $(this).data("traded");
-
-        var newTradeStatus = {
-            traded: traded
-        };
-
-        $.ajax("/api/players/" + id, {
-            type: "PUT",
-            data: newTradeStatus
-        }).then(
-            function() {
-                console.log("change traded to", newTradeStatus);
-                location.reload();
-            }
-        );
+        $(document).ready(function(){
+            $('.modal').modal();
+        $("#yes").on("click",function(){
+            var newTradeStatus = {
+                traded: traded
+            };
+            $.ajax("/api/players/" + id, {
+                type: "PUT",
+                data: newTradeStatus
+            }).then(
+                function() {
+                    console.log("change traded to", newTradeStatus);
+                    location.reload();
+                }
+            );
+        });
+        $("#no").on("click",function(){
+            location.reload();
+        });  
+        });
     });
 
     $(".create-form").on("submit", function(event){
         event.preventDefault();
+        var check = true;
+        if($("#play").val() === ""){
+            check = false;
+        }
+        else {
+            check=true;
+        }
 
+        if(check === true){
         var newPlayer = {
             name: $("#play").val().trim(),
             traded: $("[name=traded]:checked").val().trim()
@@ -35,5 +49,9 @@ $(function(){
                 location.reload();
             }
         );
+    }
+    else {
+        alert("You have not filled out player's name");
+    }
     });
 });
